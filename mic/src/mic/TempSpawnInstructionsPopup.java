e527d22092572494cc87c3787d9855d1aa6d54f1
Mu0n <micjuneau@gmail.com>
2017-02-13 03:11:15 -0500

    New button that spawns the autosquad instruction graphic on the map

---
+++ mic/src/mic/TempSpawnInstructionsPopup.java
@@ -0,0 +1,206 @@
+package mic;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Random;
+
+import javax.swing.*;
+
+
+import VASSAL.build.AbstractConfigurable;
+import VASSAL.build.Buildable;
+import VASSAL.build.GameModule;
+import VASSAL.build.module.Chatter;
+import VASSAL.build.module.GameComponent;
+import VASSAL.build.module.Map;
+import VASSAL.build.widget.PieceSlot;
+import VASSAL.command.Command;
+import VASSAL.command.CommandEncoder;
+import VASSAL.counters.GamePiece;
+
+import static mic.Util.logToChat;
+import static mic.Util.newPiece;
+
+/**
+ * Created by Mic on 13/02/2017.
+ */
+public class TempSpawnInstructionsPopup extends AbstractConfigurable implements CommandEncoder,
+        GameComponent {
+
+    private int index = 0;
+    private int minChange = 0;
+    private int maxChange = 0;
+    private Random rand = new Random();
+    private JButton tempAutoSpawnInstructionsButton;
+    private VassalXWSPieceLoader slotLoader = new VassalXWSPieceLoader();
+
+    public void addToIndex(int change) {
+        index += change;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int newIncrement() {
+        return (int) (rand.nextFloat() * (maxChange - minChange + 1))
+                + minChange;
+    }
+
+
+    private void spawnPiece(GamePiece piece, Point position) {
+        Command placeCommand = Map.getMapById("Map0").placeOrMerge(piece, position);
+        placeCommand.execute();
+        GameModule.getGameModule().sendAndLog(placeCommand);
+    }
+
+    private void tempAutoSpawnInstructionsButtonPressed() {
+
+        String instructionsGpid = "11123";
+
+        PieceSlot instrSlot = new PieceSlot();
+        GameModule mod = GameModule.getGameModule();
+        for (PieceSlot slot : GameModule.getGameModule().getAllDescendantComponentsOf(PieceSlot.class)) {
+            if (slot.getGpId().equals(instructionsGpid))
+            {
+                GamePiece gp = slot.getPiece();
+                Point pos = new Point(200, 200);
+                Command place = Map.getMapById("Map0").placeOrMerge(gp, pos);
+                place.execute();
+                mod.sendAndLog(place);
+            }
+        }
+    }
+
+    public static final String MIN = "min";
+    public static final String MAX = "max";
+
+    public void setAttribute(String key, Object value) {
+        if (MIN.equals(key)) {
+            if (value instanceof String) {
+                minChange = Integer.parseInt((String) value);
+            } else if (value instanceof Integer) {
+                minChange = ((Integer) value).intValue();
+            }
+        } else if (MAX.equals(key)) {
+            if (value instanceof String) {
+                maxChange = Integer.parseInt((String) value);
+            } else if (value instanceof Integer) {
+                maxChange = ((Integer) value).intValue();
+            }
+        }
+    }
+
+    public String[] getAttributeNames() {
+        return new String[]{MIN, MAX};
+    }
+
+    public String[] getAttributeDescriptions() {
+        return new String[]{"Minimum increment", "Maximum increment"};
+    }
+
+    public Class[] getAttributeTypes() {
+        return new Class[]{Integer.class, Integer.class};
+    }
+
+    public String getAttributeValueString(String key) {
+        if (MIN.equals(key)) {
+            return "" + minChange;
+        } else if (MAX.equals(key)) {
+            return "" + maxChange;
+        } else {
+            return null;
+        }
+    }
+
+    public void addTo(Buildable parent) {
+        GameModule mod = (GameModule) parent;
+
+        mod.addCommandEncoder(this);
+        mod.getGameState().addGameComponent(this);
+
+        tempAutoSpawnInstructionsButton = new JButton("Squad AutoSpawn Instructions");
+        tempAutoSpawnInstructionsButton.setAlignmentY(0.0F);
+        tempAutoSpawnInstructionsButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                tempAutoSpawnInstructionsButtonPressed();
+            }
+        });
+        mod.getToolBar().add(tempAutoSpawnInstructionsButton);
+
+    }
+
+    public void removeFrom(Buildable parent) {
+        GameModule mod = (GameModule) parent;
+
+        mod.removeCommandEncoder(this);
+        mod.getGameState().removeGameComponent(this);
+
+        mod.getToolBar().remove(tempAutoSpawnInstructionsButton);
+    }
+
+    public VASSAL.build.module.documentation.HelpFile getHelpFile() {
+        return null;
+    }
+
+    public Class[] getAllowableConfigureComponents() {
+        return new Class[0];
+    }
+
+    public void setup(boolean gameStarting) {
+        if (!gameStarting) {
+            index = 0;
+        }
+    }
+
+    public Command getRestoreCommand() {
+        return new Incr3(this, index);
+    }
+
+    public static final String COMMAND_PREFIX = "TENSION:";
+
+    public String encode(Command c) {
+        if (c instanceof Incr3) {
+            return COMMAND_PREFIX + ((Incr3) c).getChange();
+        } else {
+            return null;
+        }
+    }
+
+    public Command decode(String s) {
+        if (s.startsWith(COMMAND_PREFIX)) {
+            return new Incr3(this,
+                    Integer.parseInt(s.substring(COMMAND_PREFIX.length())));
+        } else {
+            return null;
+        }
+    }
+
+    public static class Incr3 extends Command {
+
+        private TempSpawnInstructionsPopup target;
+        private int change;
+
+        public Incr3(TempSpawnInstructionsPopup target, int change) {
+            this.target = target;
+            this.change = change;
+        }
+
+        protected void executeCommand() {
+            target.addToIndex(change);
+        }
+
+        protected Command myUndoCommand() {
+            return new Incr3(target, -change);
+        }
+
+        public int getChange() {
+            return change;
+        }
+    }
+}
