5c8fb8de30e294d3acd97197ade093468b1802a1
Andrew Matheny <andrew.j.matheny@gmail.com>
2017-02-17 14:58:44 -0500

    Fix issues with path x/y always being 0/0.  They are still broken

--- mic/src/mic/manuvers/CurvedPaths.java
+++ mic/src/mic/manuvers/CurvedPaths.java
@@ -8,29 +8,29 @@ import com.google.common.collect.Lists;
  * Created by amatheny on 2/17/17.
  */
 public enum CurvedPaths implements ManuverPath {
-    LBk1(80, true, true),
-    RBk1(80, false, true),
-    LBk2(130, true, true),
-    RBk2(130, false, true),
-    LBk3(180, true, true),
-    RBk3(180, false, true),
+    LBk1(80 * 5.65, true, true),
+    RBk1(80 * 5.65, false, true),
+    LBk2(130 * 5.65, true, true),
+    RBk2(130 * 5.65, false, true),
+    LBk3(180 * 5.65, true, true),
+    RBk3(180 * 5.65, false, true),
 
-    LT1(35, true, false),
-    RT1(35, false, false),
-    LT2(62.5, true, false),
-    RT2(62.5, false, false),
-    LT3(90, true, false),
-    RT3(90, false, false);
+    LT1(35 * 5.65, true, false),
+    RT1(35 * 5.65, false, false),
+    LT2(62.5 * 5.65, true, false),
+    RT2(62.5 * 5.65, false, false),
+    LT3(90 * 5.65, true, false),
+    RT3(90 * 5.65, false, false);
 
     private final boolean bank;
     private boolean left;
     private double radius;
 
-    private static double QTR_PI = Math.PI / 4;
-    private static double HALF_PI = Math.PI / 2;
+    private static double QTR_PI = Math.PI / 4.0;
+    private static double HALF_PI = Math.PI / 2.0;
 
     CurvedPaths(double radius, boolean left, boolean bank) {
-        this.radius = radius * ManuverPaths.scale;
+        this.radius = radius;
         this.left = left;
         this.bank = bank;
     }
@@ -47,12 +47,16 @@ public enum CurvedPaths implements ManuverPath {
 
         List<PathPart> parts = Lists.newArrayList();
         for (int i = 1; i <= numSegments; i++) {
-            double arg = (getPartMultipler() * i) / numSegments;
+            double arg = (getPartMultipler() * i) / (double) numSegments;
 
             double x = Math.sin(arg) * radius;
             double y = (-1 * Math.cos(arg) + 1) * radius;
-            double angleDelta = (i / (double) numSegments) * getFinalAngleOffset();
-            double angle = this.left ? angleDelta : -angleDelta;
+            double angle = -(i / (double) numSegments) * getFinalAngleOffset();
+
+            if (this.left) {
+                angle = -angle;
+                x = -x;
+            }
 
             parts.add(new PathPart(x, y, angle));
         }
--- mic/src/mic/manuvers/ManuverPaths.java
+++ mic/src/mic/manuvers/ManuverPaths.java
@@ -1,5 +1,7 @@
 package mic.manuvers;
 
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Path2D;
 import java.util.List;
 
 import com.google.common.collect.Lists;
@@ -14,7 +16,6 @@ public enum ManuverPaths {
     RT1(CurvedPaths.RT1);
 
     private static int NUM_PATH_SEGMENTS = 100;
-    public static double scale = 5.65;
 
     private final ManuverPath path;
 
@@ -25,23 +26,60 @@ public enum ManuverPaths {
     public List<PathPart> getTransformedPathParts(double x, double y, double angleDegrees) {
         List<PathPart> rawParts = this.path.getPathParts(NUM_PATH_SEGMENTS);
         List<PathPart> transformed = Lists.newArrayList();
+
+        Path2D.Double path = new Path2D.Double();
+        path.moveTo(0, 0);
+
         for (PathPart rawPart : rawParts) {
-            int angle = (int) Math.floor(angleDegrees + rawPart.getAngle());
+            Path2D.Double testPath = new Path2D.Double();
+            testPath.moveTo(rawPart.getX(), rawPart.getY());
+            testPath = (Path2D.Double) AffineTransform
+                    .getRotateInstance(-angleDegrees * 180 / Math.PI, 0, 0)
+                    .createTransformedShape(testPath);
+
+            double angle = angleDegrees + rawPart.getAngle();
             if (angle > 0) {
                 angle = angle - 360;
             }
-
-            double rotatedPartX = rawPart.getX() * Math.cos(rawPart.getAngle())
-                    - rawPart.getY() * Math.sin(rawPart.getAngle());
-            double rotatedPartY = rawPart.getY() * Math.cos(rawPart.getAngle())
-                    + rawPart.getX() * Math.sin(rawPart.getAngle());
-
             transformed.add(new PathPart(
-                    rotatedPartX + x,
-                    rotatedPartY + y,
+                    x + testPath.getCurrentPoint().getX(),
+                    y + testPath.getCurrentPoint().getY(),
                     angle
             ));
+
+            path.lineTo(rawPart.getX(), rawPart.getY());
         }
+
+//        path = (Path2D.Double) AffineTransform.getRotateInstance(angleDegrees * 180 / Math.PI, 0, 0).createTransformedShape(path);
+//
+//        PathIterator iterator = path.getPathIterator(null);
+//        iterator.next();
+//        int i = 0;
+//        double[] coords = new double[6];
+//        while (!iterator.isDone()) {
+//            PathPart rawPart = rawParts.get(i);
+//            double angle = angleDegrees + rawPart.getAngle();
+//            if (angle > 0) {
+//                angle = angle - 360;
+//            }
+//
+//            iterator.currentSegment(coords);
+//
+//            transformed.add(new PathPart(
+//                    x + coords[0],
+//                    y + coords[1],
+//                    angle
+//            ));
+//            iterator.next();
+//        }
         return transformed;
+    }
+
+    public static void main(String[] args) {
+        List<PathPart> parts = ManuverPaths.RT1.getTransformedPathParts(0, 0, 0);
+//        List<PathPart> parts = CurvedPaths.LBk1.getPathParts(100);
+        for (PathPart part : parts) {
+            System.out.println(String.format("%s\t %s", part.getX(), part.getY(), part.getAngle()));
+        }
     }
 }
