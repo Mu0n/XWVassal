5334d781cb3218f1df1c78fdfa26cf9ff7810e6e
Mu0n <micjuneau@gmail.com>
2017-08-24 10:34:34 -0400

    trying to start up a proof of concept class for a ship mouse GUI

---
+++ mic/src/mic/MouseShipGUI.java
@@ -0,0 +1,82 @@
+package mic;
+
+import VASSAL.build.module.documentation.HelpFile;
+import VASSAL.command.Command;
+import VASSAL.counters.Decorator;
+import VASSAL.counters.EditablePiece;
+import VASSAL.counters.GamePiece;
+import VASSAL.counters.KeyCommand;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Mic on 09/08/2017.
+ *
+ * This source file manages every mouse event so that the ships can be driven by a non-modal mouse interface with buttons
+ */
+public class MouseShipGUI extends Decorator implements EditablePiece {
+    public static final String ID = "MouseShipGUI";
+
+    public MouseShipGUI(){
+        this(null);
+    }
+
+    public MouseShipGUI(GamePiece piece){
+        setInner(piece);
+    }
+
+    @Override
+    public Command keyEvent(KeyStroke stroke) {
+        return piece.keyEvent(stroke);
+    }
+
+    @Override
+    public void mySetState(String s) {
+
+    }
+    @Override
+    public String myGetState() {
+        return "";
+    }
+    @Override
+    public String myGetType() {
+        return ID;
+    }
+    @Override
+    protected KeyCommand[] myGetKeyCommands() {
+        return new KeyCommand[0];
+    }
+    @Override
+    public Command myKeyEvent(KeyStroke keyStroke) {
+        return null;
+    }
+
+    public String getDescription() {
+        return "MouseShipGUI";
+    }
+
+    public void mySetType(String s) {
+
+    }
+
+    public HelpFile getHelpFile() {
+        return null;
+    }
+
+    public void draw(Graphics graphics, int i, int i1, Component component, double v) {
+
+    }
+
+    public Rectangle boundingBox() {
+        return this.piece.boundingBox();
+    }
+
+    public Shape getShape() {
+        return this.piece.getShape();
+    }
+
+    public String getName() {
+        return this.piece.getName();
+    }
+}
