975e860fe131dd0a663acbe3b022184d3d2b5978
mjuneau <micjuneau@gmail.com>
2017-12-21 15:23:00 -0500

    shorcut changed again, still doesn't trigger. hard hello world for no good reason

--- mic/src/mic/StemDial.java
+++ mic/src/mic/StemDial.java
@@ -55,9 +55,9 @@ public class StemDial extends Decorator implements EditablePiece {
     public Command myKeyEvent(KeyStroke keyStroke) {
 
         //check to see if 'x' was pressed
-        if(KeyStroke.getKeyStroke(KeyEvent.VK_C, 0, false).equals(keyStroke)) {
+        if(KeyStroke.getKeyStroke(KeyEvent.VK_I, KeyEvent.CTRL_DOWN_MASK, false).equals(keyStroke)) {
 
-           logToChatWithTime("c was pressed");
+           logToChatWithTime("ctrl-i was pressed");
         }
 
         return piece.keyEvent(keyStroke);
