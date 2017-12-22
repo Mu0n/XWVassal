f359dece867a40e6b22c4ceec9ce658a66a0fc74
mjuneau <micjuneau@gmail.com>
2017-12-22 16:02:57 -0500

    trying to add the framework for a dial generator command

--- mic/src/mic/StemDial.java
+++ mic/src/mic/StemDial.java
@@ -2,7 +2,10 @@ package mic;
 
 import VASSAL.build.module.documentation.HelpFile;
 import VASSAL.command.Command;
+import VASSAL.command.CommandEncoder;
 import VASSAL.counters.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.awt.*;
@@ -47,24 +50,21 @@ public class StemDial extends Decorator implements EditablePiece {
     }
     @Override
     public Command myKeyEvent(KeyStroke keyStroke) {
-return null;
+        return null;
     }
 
     @Override
     public Command keyEvent(KeyStroke stroke) {
         //check to see if 'x' was pressed
         if(KeyStroke.getKeyStroke(KeyEvent.VK_I, KeyEvent.CTRL_DOWN_MASK, true).equals(stroke)) {
-            MasterShipData.ShipData shipData = MasterShipData.getShipData(shipXWS);
+            logToChatWithTime("temporary trigger for Dial generation -will be eventually ported to autospawn\nPossibly to a right click menu as well with a dynamically fetched list of all ships??");
 
-            List<List<Integer>> moveList = shipData.getManeuvers();
+            DialGenerateCommand myDialGen = new DialGenerateCommand("xwing");
+            Command stringOCommands = piece.keyEvent(stroke);
+            stringOCommands.append(myDialGen);
 
-            for(List<Integer> li : moveList) {
-                for(Integer i : li) {
-                    logToChatWithTime(i.toString());
-                }
-            }
-
-
+            myDialGen.execute();
+            return stringOCommands;
         }
 
         return piece.keyEvent(stroke);
@@ -96,4 +96,85 @@ return null;
     public String getName() {
         return this.piece.getName();
     }
+
+
+    //this is the command that takes a ship xws name, fetches the maneuver info and constructs the dial layer by layer
+    public static class DialGenerateCommand extends Command {
+        static String xwsShipName = "";
+        List<List<Integer>> moveList; //possibly changed for a higher level class so it stays current with future xws spec changes
+
+        DialGenerateCommand(String thisName) {
+
+            // fetch the maneuver array of arrays according to the xws name passed on from autospawn or other means
+            xwsShipName = thisName;
+            MasterShipData.ShipData shipData = MasterShipData.getShipData(xwsShipName);
+            moveList = shipData.getManeuvers();
+
+
+        }
+
+        protected void executeCommand() {
+            // construct the dial Layers trait (Embellishment class) layer by layer according to the previous Array of Arrays.
+
+            //Fetch the existing Embellishment and pass it through a customLayers instance? Not clear here
+            //customLayers myCustomLayers = new customLayers();
+            //
+            logToChat("execute command = current ship xws name is: " + xwsShipName);
+        }
+
+        protected Command myUndoCommand() {
+            return null;
+        }
+
+
+
+
+        //the following class is used to send the info to the other player whenever a dial generation command is issued, so it can be done locally on all machines playing/watching the game
+        //only the ship XWS string is sent
+        public static class DialGeneratorEncoder implements CommandEncoder {
+            private static final Logger logger = LoggerFactory.getLogger(StemDial.class);
+            private static final String commandPrefix = "DialGeneratorEncoder=";
+
+            public static StemDial.DialGenerateCommand.DialGeneratorEncoder INSTANCE = new StemDial.DialGenerateCommand.DialGeneratorEncoder();
+
+            public Command decode(String command) {
+                if (command == null || !command.contains(commandPrefix)) {
+                    return null;
+                }
+                logger.info("Decoding DialGenerateCommand");
+
+                command = command.substring(commandPrefix.length());
+                try {
+                    xwsShipName = command.toString();
+                } catch (Exception e) {
+                    logger.error("Error decoding DialGenerateCommand", e);
+                    return null;
+                }
+                return null;
+            }
+
+            public String encode(Command c) {
+                if (!(c instanceof DialGenerateCommand)) {
+                    return null;
+                }
+                logger.info("Encoding DialGenerateCommand");
+                DialGenerateCommand dialGenCommand = (DialGenerateCommand) c;
+                try {
+                    return commandPrefix + xwsShipName;
+                } catch(Exception e) {
+                    logger.error("Error encoding DialGenerateCommand", e);
+                    return null;
+                }
+            }
+        }
+
+    }
+
+    //this is used to access the Layers trait (or Embellishment class) and circumvent the protection for its methods
+    //should be used by DialGenerateCommand while a dial is being constructed
+    public static class customLayers extends Embellishment {
+
+    }
+
+
 }
