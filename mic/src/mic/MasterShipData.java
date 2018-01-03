335a69c33b4768a1ece033b5def7993df9bd221b
mrmurphm <mm48080@gmail.com>
2018-01-03 13:31:20 -0600

    Removed test ship and debugging code.

--- mic/src/mic/MasterShipData.java
+++ mic/src/mic/MasterShipData.java
@@ -38,52 +38,12 @@ public class MasterShipData extends ArrayList<MasterShipData.ShipData> {
         // add in any ships from dispatcher that aren't in xwing-data
         for(ShipData ship : dispatcherData)
         {
-            //TODO DELETE ME
-            Util.logToChat("Checking ship "+ship.getXws());
             if(loadedData.get(ship.getXws()) == null)
             {
                 Util.logToChat("Adding ship "+ship.getXws()+" from dispatcher file");
                 loadedData.put(ship.getXws(),ship);
             }
         }
-
-  /*
-        // load from xwing-data
-        MasterShipData data = Util.loadRemoteJson(REMOTE_URL, MasterShipData.class);
-        if (data == null) {
-            Util.logToChat("Unable to load xwing-data for ships from the web, falling back to local copy");
-            data = Util.loadClasspathJson("ships.json", MasterShipData.class);
-        }
-
-
-        // MrMurphM
-        // Load the local copy of new ship maneuvers
-        // if any of the ships from the XWing Data don't have the new maneuvers, pull them from the local copy
-
- //       MasterShipData maneuversData = Util.loadClasspathJson("maneuvers.json", MasterShipData.class);
-
-
-        loadedData = Maps.newHashMap();
-        for(ShipData ship : data) {
-            // if the ship doesn't have the new dial maneuvers, pull it from the local version
-         //   if(ship.getDialManeuvers() == null || ship.getDialManeuvers().size() == 0)
-        //    {
-         //       Util.logToChat("Ship %s doesn't have dial maneuvers in xwing-data, reading from local copy", ship.getXws());
-         //       for(ShipData localShip : maneuversData )
-         //       {
-          //          if(localShip.getXws().equalsIgnoreCase(ship.getXws()))
-          //          {
-          //              // this is the ship
-         //               ship.setDialManeuvers(localShip.getDialManeuvers());
-         //               break;
-          //          }
-         //       }
-          //  }
-            loadedData.put(ship.getXws(), ship);
-        }
-*/
-
-
     }
 
     private static void loadFromXwingData()
