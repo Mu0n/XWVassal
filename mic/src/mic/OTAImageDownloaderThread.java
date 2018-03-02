73a1334551bd6dbfac73aaf48155cc123d815a5d
mrmurphm <mm48080@gmail.com>
2018-03-02 12:25:00 -0600

    temp

---
+++ mic/src/mic/OTAImageDownloaderThread.java
@@ -0,0 +1,41 @@
+package mic;
+
+import java.util.ArrayList;
+
+public class OTAImageDownloaderThread implements Runnable {
+//    private OTAImageDownloader imageDownloader;
+
+    public OTAImageDownloaderThread()
+    {
+
+    }
+
+    private void executeWork(ArrayList<String> pilotImageDownloadList)
+    {
+        int percent = 0;
+        int total = pilotImageDownloadList.size();
+        int done = 0;
+        String text = "";
+        for(String pilotImage: pilotImageDownloadList)
+        {
+            Util.logToChat("OTAIDT: Downloading "+pilotImage);
+            text = "Downloading "+pilotImage;
+            OTAImageDownloader.updateProgress(percent,text);
+            Util.downloadAndSaveImageFromOTA("pilots",pilotImage);
+            Util.logToChat("OTAIDT: Download Complete: "+pilotImage);
+            done++;
+
+            percent = (done * 100/total) ;
+            OTAImageDownloader.updateProgress(percent,text);
+           // Thread.yield();
+
+        }
+        OTAImageDownloader.setComplete(true);
+    }
+
+
+    @Override
+    public void run() {
+        executeWork(OTAImageDownloader.getPilotImageDownloadList());
+    }
+}
