d1da921205cc8affff1be3e8dc527703d4f2a5cf
Mu0n <micjuneau@gmail.com>
2018-08-29 01:25:41 -0400

    more alt ships

--- mic/src/mic/AltArtShipPicker2e.java
+++ mic/src/mic/AltArtShipPicker2e.java
@@ -16,20 +16,20 @@ public class AltArtShipPicker2e {
     private static Map<String,String> newPilotAltArts = ImmutableMap.<String, String>builder()
             .put("blacksunace","blacksunace")
             .put("emonazzameen","emon")
+            .put("krassistrelix","krassis")
+            .put("greensquadronpilot","green")
+            .put("mirandadoni","miranda")
             /*
             .put("prototypepilot","blue")
-            .put("greensquadronpilot","green")
             .put("bluesquadronpilot","blue")
             .put("daggersquadronpilot","dagger")
             .put("knavesquadronpilot","knave")
             .put("kathscarlet","kath")
             .put("mandalorianmercenary","merc")
             .put("bountyhunter","bountyhunter")
-            .put("krassistrelix","krassis")
             .put("grazthehunter","graz")
             .put("esegetuketu","esege")
             .put("guardiansquadronpilot","guardian")
-            .put("mirandadoni","miranda")
             .put("laetinashera","laetin")
             .put("serissu","serissu")
             .put("poedameron","blackone")
@@ -53,59 +53,6 @@ public class AltArtShipPicker2e {
             */
             .build();
 
-    private static Map<String,String> defaultShipArts = ImmutableMap.<String, String>builder()
-            .put("rebelalliance_awing","red")
-            .put("rebelalliance_tiefighter","sabinestie")
-            .put("rebelalliance_ywing","gold")
-            .put("scumandvillainy_ywing","scum")
-            .put("scumandvillainy_z95headhunter","blacksun")
-            .put("rebelalliance_z95headhunter","rebel").build();
-
-   // private static Map<String,String[]> dualBaseShips = ImmutableMap.<String, String[]>builder()
-   //         .put("uwing",UWING_ART).build();
-
-
-
-    private static Map<String, String> pilotAltArts = ImmutableMap.<String, String>builder()
-            .put("greensquadronpilot", "Green Squadron")
-            .put("prototypepilot", "Prototype")
-            .put("bluesquadronpilot", "B-Wing Blue")
-            .put("daggersquadronpilot", "B-Wing Dagger")
-            .put("knavesquadronpilot", "Knave")
-            .put("guardiansquadronpilot", "Guardian")
-            .put("esegetuketu", "Esege")
-            .put("miranda", "Miranda")
-            .put("lukeskywalker", "Luke")
-            .put("graysquadronpilot", "Gray")
-            .put("poedameron", "Black One")
-            .put("poedameron-swx57", "Black One")
-            .put("wildspacefringer", "Fringer")
-            .put("eadenvrill", "Eaden")
-            .put("banditsquadronpilot", "Bandit")
-            .put("talasquadronpilot", "Tala")
-            .put("airencracken", "Cracken")
-            .put("lieutenantblount", "Blount")
-            .put("bountyhunter", "Bounty")
-            .put("krassistrelix", "Krassis")
-            .put("kathscarlet", "Kath")
-            .put("countessryad", "Veteran")
-            .put("tomaxbren", "Veteran")
-            .put("gammasquadronpilot", "Veteran")
-            .put("soontirfel", "181st")
-            .put("royalguardpilot", "Royal")
-            .put("emonazzameen", "Emon")
-            .put("mandalorianmercenary", "Mandalorian")
-            .put("grazthehunter", "Graz")
-            .put("blacksunace", "Black Sun")
-            .put("laetinashera", "Laetin")
-            .put("serissu", "Serissu")
-            .put("binayrepirate", "Binayre")
-            .put("blacksunsoldier", "Black Sun")
-            .put("kaatoleeachos", "Leeachos")
-            .put("ndrusuhlak", "Suhlak")
-            .put("nashtahpuppilot", "Nashtah")
-            .build();
-
 
     public static String getNewAltArtShip(String xwsPilot, String xwsShip, String faction)
     {
@@ -120,7 +67,7 @@ public class AltArtShipPicker2e {
             altArt = normalAltArt;
             found = true;
         }
-
+/*
         // now check for faction/ship defaults
         if(!found)
         {
@@ -134,7 +81,7 @@ public class AltArtShipPicker2e {
             }
         }
 
-
+*/
         // if we still haven't found anything, then there is no alt art
         if(!found)
         {
