4082eabd7496978ae4eff19a158c7c0fe9cd9e7d
mjuneau <micjuneau@gmail.com>
2019-03-20 12:49:28 -0400

    start up on launched droids

--- mic/src/mic/BombSpawner.java
+++ mic/src/mic/BombSpawner.java
@@ -47,7 +47,9 @@ enum BombToken {
     SeismicCharge("Seismic Charge", "Bomb", "3665", 0.0f, 40.0f),
     ProtonBomb("Proton Bomb", "Bomb", "1269", 0.0f, 40.0f),
     ThermalDetonator("Thermal Detonator", "Bomb", "8867", 0.0f, 40.0f),
-    Bomblet("Bomblet", "Bomb", "11774", 0.0f, 40.0f);
+    Bomblet("Bomblet", "Bomb", "11774", 0.0f, 40.0f),
+    BuzzDroidSwarm("Buzz Droid Swarm", "Mine", "", 0.0f, 40.0f),
+    DRK1ProbeDroid("DRK-1 Probe Droid", "Remote", "", 0.0f, 40.0f);
 
     private final String bombName;
     private final String bombType;
