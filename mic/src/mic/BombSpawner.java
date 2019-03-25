2e4d30975914068a217abaeb16d1c62b4e28d6a7
mjuneau <micjuneau@gmail.com>
2019-03-25 13:24:52 -0400

    associate spare parts to the  bomb spawner

--- mic/src/mic/BombSpawner.java
+++ mic/src/mic/BombSpawner.java
@@ -50,7 +50,8 @@ enum BombToken {
     Bomblet("Bomblet", "Bomb", "11774", 0.0f, 40.0f),
     BuzzDroidSwarm("Buzz Droid Swarm", "Remote", "13069", 0.0f, 53.0f),
     DRK1ProbeDroid("DRK-1 Probe Droid", "Remote", "13068", 0.0f, 62.0f),
-    CargoDebris("Cargo", "Debris", "12871", 0.0f, 82.5f);
+    CargoDebris("Cargo", "Debris", "12871", 0.0f, 82.5f),
+    SpareParts("Spare Parts", "Debris", "", 0.0f, -62.5f);
 
     private final String bombName;
     private final String bombType;
