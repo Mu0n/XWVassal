3c089036b5bd1d45ec08bd176930091912eb5db2
mjuneau <micjuneau@gmail.com>
2019-04-08 00:15:02 -0400

    better filled in methods for editablepiece

--- mic/src/mic/RemoteRelocation.java
+++ mic/src/mic/RemoteRelocation.java
@@ -364,20 +364,20 @@ public class RemoteRelocation extends Decorator implements EditablePiece {
         return null;
     }
 
-    public void draw(Graphics g, int x, int y, Component obs, double zoom) {
-
+    public void draw(Graphics graphics, int i, int i1, Component component, double v) {
+        this.piece.draw(graphics, i, i1, component, v);
     }
 
     public Rectangle boundingBox() {
-        return null;
+        return this.piece.boundingBox();
     }
 
     public Shape getShape() {
-        return null;
+        return this.piece.getShape();;
     }
 
     public String getName() {
-        return null;
+        return this.piece.getName();
     }
 
     public static RemoteRelocation findRemoteRelocationDecorator(GamePiece activatedPiece) {
