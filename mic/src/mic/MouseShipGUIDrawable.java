e985ccfb5900356c0a64d7506a48def03dbd0d29
mjuneau <micjuneau@gmail.com>
2019-07-10 23:06:22 -0400

    ccw rotation

--- mic/src/mic/MouseShipGUIDrawable.java
+++ mic/src/mic/MouseShipGUIDrawable.java
@@ -197,7 +197,7 @@ public class MouseShipGUIDrawable extends MouseGUIDrawable implements Drawable {
         // *
 
         //ship rotations
-        MouseShipGUIElement ROT180 = new MouseShipGUIElement(4,"TOG_ARC", "mi_180.png", 295, 100,
+        MouseShipGUIElement ROT180 = new MouseShipGUIElement(4,"TOG_ARC", "mi_180.png", 217, 86,
                 KeyStroke.getKeyStroke(KeyEvent.VK_U, KeyEvent.CTRL_DOWN_MASK, false), 0," flip around 180 degrees");
 
         MouseShipGUIElement CW1 = new MouseShipGUIElement(4,"TOG_TUR", "mi_cw_1.png", 321, 206,
@@ -217,9 +217,9 @@ public class MouseShipGUIDrawable extends MouseGUIDrawable implements Drawable {
         MouseShipGUIElement DISPLACERIGHT = new MouseShipGUIElement(4, "ARROW_RIGHT", "mi_arrow_right.png", 458,64,
                 KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, KeyEvent.SHIFT_DOWN_MASK, false), -999," move right 1 pixel");
 
-        MouseShipGUIElement LAUNCH = new MouseShipGUIElement(4, "TOG_ARC", "mi_launch_device.png", 116,5,
+        MouseShipGUIElement LAUNCH = new MouseShipGUIElement(4, "TOG_ARC", "mi_launch_device.png", 72,19,
                 KeyStroke.getKeyStroke(KeyEvent.VK_B, KeyEvent.CTRL_DOWN_MASK + KeyEvent.SHIFT_DOWN_MASK, false), 0," launch a device");
-        MouseShipGUIElement DROP = new MouseShipGUIElement(4, "TOG_BR", "mi_drop_device.png", 116,73,
+        MouseShipGUIElement DROP = new MouseShipGUIElement(4, "TOG_BR", "mi_drop_device.png", 73,86,
                 KeyStroke.getKeyStroke(KeyEvent.VK_B, KeyEvent.CTRL_DOWN_MASK, false), 0," drop a device");
 
         MouseShipGUIElement CONPAGE = new MouseShipGUIElement(0,"TOG_CON", "Toggle_Conditions_Active.png", "Toggle_Conditions_Inactive.png",295, 397, null, -196);
