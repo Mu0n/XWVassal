8f14c9f54a1981e963506efd1c5720e902e17b72
Mu0n <micjuneau@gmail.com>
2021-09-25 08:13:45 -0400

    electro-chaff cloud ranges, will add token in editor

--- mic/src/mic/AutoRangeForTokens.java
+++ mic/src/mic/AutoRangeForTokens.java
@@ -58,7 +58,8 @@ enum ImagesUsedForRanges {
     IonBombs("Mine_Explosion","Mine_Explosion2","Mine_Explosion3",0 ,0),
     Bomblet("Mine_Explosion","Mine_Explosion2","Mine_Explosion3",0 ,0),
     ElectroProton("Mine_Explosion","Mine_Explosion2","Mine_Explosion3",0,0),
-    Sensorbuoy("Buoy-range1","Buoy-range2","Buoy-range",0,0);
+    Sensorbuoy("Buoy-range1","Buoy-range2","Buoy-range",0,0),
+    ElectroCloudChaff("Electro-Chaff_Cloud_r1.png", "Electro-Chaff_Cloud_r2.png","Electro-Chaff_Cloud_r3.png",0,0);
 
     private final String r1Img;
     private final String r2Img;
@@ -145,6 +146,7 @@ public class AutoRangeForTokens extends Decorator implements EditablePiece {
             .put("Electro-Proton Bomb", ImagesUsedForRanges.ElectroProton)
             .put("Sensor Buoy (Red) Token",ImagesUsedForRanges.Sensorbuoy)
             .put("Sensor Buoy (Blue) Token",ImagesUsedForRanges.Sensorbuoy)
+            .put("Electro-Chaff Cloud",ImagesUsedForRanges.ElectroCloudChaff)
             .build();
 
     public AutoRangeForTokens() {
